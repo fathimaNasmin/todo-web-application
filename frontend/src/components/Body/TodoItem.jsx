ecec4aef85bdc45c3df97cd3a9886ee2e8588c26
Fathima nasmin <fathimanesmi@gmail.com>
2024-05-10 17:38:15 -0400

    add container for time when the task was created.

--- frontend/src/components/Body/TodoItem.jsx
+++ frontend/src/components/Body/TodoItem.jsx
@@ -4,10 +4,17 @@ import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 import shortid from "shortid";
 
 export default function TodoItem({ item }) {
+    const day = item.createdAt.getDate();
+    const month = item.createdAt.getMonth() + 1; // Adding 1 to get the correct month number
+    const year = item.createdAt.getFullYear();
   return (
     <div className={styles.todoItem}>
       <input className={styles.checkbox} type="checkbox" />
-      <label>{item.name}</label>
+      <div className={styles.itemContent}>
+        <label>{item.name}</label>
+        <p>created on:{day}-{month}-{year}</p>
+      </div>
+
       <button>
         <FontAwesomeIcon icon={faTrashCan} size="xs" />
       </button>
