50cb76248bfc5cf62304178f3401b0aabd5982eb
Fathima nasmin <fathimanesmi@gmail.com>
2024-05-18 16:56:01 -0400

    added login page

--- frontend/src/components/Body/Signup.jsx
+++ frontend/src/components/Body/Signup.jsx
@@ -1,21 +1,35 @@
-import styles from './signup.module.css'
-export default function Signup(){
-    return (
-      <div className={styles.bg}>
-        <div className={styles.signupContainer}>
-          <h1>Signup</h1>
-          <form action="" className={styles.signUpBox}>
-            <input type="text" placeholder="Name" />
-            <input type="text" placeholder="Email" />
-            <input type="password" placeholder="Password" />
+import { useState } from "react";
+import styles from "./signup.module.css";
+
+export default function Signup() {
+  const [signState, setSignState] = useState(true);
+  return (
+    <div className={styles.bg}>
+      <div className={styles.signupContainer}>
+        <h1>{signState ? "Signup" : "Login"}</h1>
+        <form action="" className={styles.signUpBox}>
+          {signState ? <input type="text" placeholder="Name" /> : ""}
+
+          <input type="text" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          {signState ? (
             <input type="password" placeholder="Confirm Password" />
-            <button>Signup</button>
-          </form>
+          ) : (
+            ""
+          )}
+
+          <button>{signState ? "Signup" : "Login"}</button>
+        </form>
+        {signState ? (
           <p>
-            Already have account?{" "}
-            <span>Login In</span>
+            Already have account? <span onClick={()=>{setSignState(false)}}>Login In</span>
           </p>
-        </div>
+        ) : (
+          <p>
+            Don't have Account? <span onClick={()=>setSignState(true)}>Sign Up</span>
+          </p>
+        )}
       </div>
-    );
+    </div>
+  );
 }
