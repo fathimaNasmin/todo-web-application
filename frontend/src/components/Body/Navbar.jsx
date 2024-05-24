956e657f7971879de6357694012cd617dd998882
Fathima nasmin <fathimanesmi@gmail.com>
2024-05-24 11:10:56 -0400

    implement signout

--- frontend/src/components/Body/Navbar.jsx
+++ frontend/src/components/Body/Navbar.jsx
@@ -1,10 +1,14 @@
 import styles from "./navbar.module.css";
+import { useContext } from "react";
+import { AuthContext } from "../Hooks/authContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPowerOff } from "@fortawesome/free-solid-svg-icons";
 
 export default function Navbar() {
+    const {isAuthenticated, token, setAuth} = useContext(AuthContext);
     const handleSignOut = () =>{
-        console.log("signout");
+        console.log("signout:", token);
+        setAuth(null, false)
     }
   return (
     <div className={styles.navContainer}>
