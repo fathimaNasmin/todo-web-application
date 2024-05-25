f6bf37f8bb8b6d2928c91ec4bcd78cf4b1bc8727
Fathima nasmin <fathimanesmi@gmail.com>
2024-05-25 16:53:25 -0400

    initial api call for user Info

--- frontend/src/components/pages/TodoPage/TodoPage.jsx
+++ frontend/src/components/pages/TodoPage/TodoPage.jsx
@@ -1,46 +1,52 @@
 import Body from "../../Body/Body";
 import Footer from "../../Body/Footer";
 import Navbar from "../../Body/Navbar";
-import '../../../App.css';
+import "../../../App.css";
 import { DarkModeContext } from "../../Hooks/useDarkMode";
 import { useContext, useEffect } from "react";
-import axiosInstance from '../../../api';
-import {userInfoUrl, taskUrl} from '../../../urls'
+import axiosInstance from "../../../api";
+import { userInfoUrl, taskUrl } from "../../../urls";
 import { AuthContext } from "../../Hooks/authContext";
 
 export default function TodoPage() {
   // Use custom context to access value.
-  const {isDark} = useContext(DarkModeContext);
+  const { isDark } = useContext(DarkModeContext);
   const { token, setCurrentUser } = useContext(AuthContext);
 
-  useEffect(()=>{
-    // api request with token to get user info
-    axiosInstance
-      .get(userInfoUrl, {
-        headers: {
-          'Authorization': token,
-        },
-      })
-      .then((response) => {
-        let userInfo = response.data;
-        setCurrentUser(userInfo)
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+  useEffect(() => {
+    if (token) {
+      const fetchData = () => {
+        // api request with token to get user info
+        axiosInstance
+          .get(userInfoUrl, {
+            headers: {
+              Authorization: token,
+            },
+          })
+          .then((response) => {
+            let userInfo = response.data;
+            setCurrentUser(userInfo);
+            console.log(userInfo);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      };
+      fetchData();
+    }
 
     // GET api
-    axiosInstance
-      .get("http://localhost:8000/api/task/", {
-        headers: {
-          'Authorization': token,
-        },
-      })
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((error) => console.log(error));
-  },[])
+    // axiosInstance
+    //   .get("http://localhost:8000/api/task/", {
+    //     headers: {
+    //       'Authorization': token,
+    //     },
+    //   })
+    //   .then((response) => {
+    //     console.log(response);
+    //   })
+    //   .catch((error) => console.log(error));
+  }, []);
 
   return (
     <>
